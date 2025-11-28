8e6671b03793502d1fbe221a323205ec78e58e80
jean <jeanguilherme25@gmail.com>
2025-11-27 23:22:08 -0300

    cria funcaoSoma e funcaoMedia

--- Algoritmo_Soma_Media/src/Main.java
+++ Algoritmo_Soma_Media/src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,16 +15,31 @@ public class Main {
             numeros[i] = lerInteiro(sc);
         }
 
-        // Parte 1 (Integrante 1): Soma
+        
         int soma = funcaoSoma(numeros);
         System.out.println("Soma dos números = " + soma);
 
-        // Parte 2 (Integrante 2): Média
+        
         double media = funcaoMedia(numeros);
         System.out.println("Média dos números = " + media);
 
     }
 
     
+    public static int funcaoSoma(int[] valores) {
+        int soma = 0;
+        for (int v : valores) {
+            soma += v;
+        }
+        return soma;
+    }
+
+    public static double funcaoMedia(int[] valores) {
+        int soma = 0;
+        for (int v : valores) {
+            soma += v;
+        }
+        return soma / 5.0;
+    }
 
 }
