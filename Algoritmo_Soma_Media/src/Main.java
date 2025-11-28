ff418c410edaf2991ff057ea948b8b4628272c50
douglasrafael100 <douglas-rafael10@hotmail.com>
2025-11-27 22:12:06 -0300

    Cria classe Main

--- Algoritmo_Soma_Media/src/Main.java
+++ Algoritmo_Soma_Media/src/Main.java
@@ -2,9 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numeros = new int[5];
+
+        System.out.println("=== Informe 5 números inteiros ===");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = lerInteiro(sc);
+        }
+
+        // Parte 1 (Integrante 1): Soma
+        int soma = funcaoSoma(numeros);
+        System.out.println("Soma dos números = " + soma);
+
+        // Parte 2 (Integrante 2): Média
+        double media = funcaoMedia(numeros);
+        System.out.println("Média dos números = " + media);
 
     }
 
-
+    
 
 }
